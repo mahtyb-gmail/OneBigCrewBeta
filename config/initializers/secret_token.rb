4571d92d5b6e01f1f5801ebfb031b5c05cd38cb2
Martin Burke <mahtyb@gmail.com>
2013-06-28 15:12:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneBigCrewBeta::Application.config.secret_token = '3895793be3885335ffc93514bcba1d0995b101f2855147f32e8715da63d3c6ceaf357c56858d7791a96f03aea5ecbec98f8eea5ebd44650b67566bde2fab696a'
